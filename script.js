c0a1ce57856314b16dcfedbd940327c7a5307aaf
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-05-19 12:17:37 +0200

    turn

--- script.js
+++ script.js
@@ -1,29 +1,67 @@
-// personnages 
+// ----------------------------personnages 
 
 let perso1 = {
     name: "A",
     energie: 10,
     vies: 5,
-    points: 0
+    points: 0,
+    turn: 0
 }
 
 let perso2 = {
     name: "B",
     energie: 10,
     vies: 5,
-    points: 0
+    points: 0,
+    turn: 0
 }
 
+//----------------------changer de jouer
 
-function Attaque(degats, cible, vie) {
-    if (cible.energie > 0) {
-        cible.energie = cible.energie - degats;
+function ChangeJoueur() {
+    if (perso1.turn % 2 === 0) {
+        alert("tour du joueur 1");
     } else {
-        cible.vies = cible.vies - vie;
+        alert("tour du joueur 2");
+    }
+}
+
+//---------------------------ajouter tour
+
+function addTurn(perso) {
+    perso.turn = perso.turn + 1;
+}
+
+//-----------------------------attaques
+
+function Uppercut(cible) {
+    if (cible.energie > 0) {
+        cible.energie = cible.energie - 1;
+    } else {
+        cible.vies = cible.vies - 1;
         cible.energie = 10;
     }
 }
 
+function Direct(cible) {
+    if (cible.energie >= 4) {
+        cible.energie = cible.energie - 4;
+    } else {
+        cible.vies = cible.vies - 1;
+        cible.energie = 10;
+    }
+}
+
+function Crochet(cible) {
+    if (cible.energie >= 2) {
+        cible.energie = cible.energie - 2;
+    } else {
+        cible.vies = cible.vies - 1;
+        cible.energie = 10;
+    }
+}
+
+//----------------------------afficher énergie et vies 
 
 function afficherEVJ1() {
     let pointsener = document.querySelector(".compteurener1");
@@ -39,9 +77,13 @@ function afficherEVJ2() {
     pointsvies.innerHTML = perso2.vies;
 }
 
+// -----------------------------ajouter points
+
 function Points(personnage) {
     personnage.points = personnage.points + 1;
 }
+
+//-----------------réinitialiser l'énergie et les vies
 
 function ReinitialiserJ1() {
     perso1.energie = 10;
@@ -53,6 +95,8 @@ function ReinitialiserJ2() {
     perso2.vies = 5;
 }
 
+//------------------------- game over
+
 function GameOver(personnage, cible) {
     if (cible.vies < 0) {
         Points(personnage);
@@ -61,20 +105,26 @@ function GameOver(personnage, cible) {
         afficherEVJ1();
         ReinitialiserJ2();
         afficherEVJ2();
+        addTurn(perso1);
+        addTurn(perso2);
+        ChangeJoueur();
     }
 }
+
+//------------------ afficher le score du joueur 1
 
 function AfficherPoints1() {
     let pointJ1 = document.querySelector(".compteurJ1");
     pointJ1.innerHTML = perso1.points;
 }
 
+// afficher le score du joueur 2
 function AfficherPoints2() {
     let pointJ2 = document.querySelector(".compteurJ2");
     pointJ2.innerHTML = perso2.points;
 }
 
-//animations
+//----------------------------------animations
 
 function tremblement1() {
     let personnage1 = document.querySelector(".perso1");
@@ -118,14 +168,18 @@ function eclair2() {
     })
 }
 
+
+//----------------------------------- événements
+
 afficherEVJ1();
 afficherEVJ2();
+ChangeJoueur();
 
-//event joueur 1
+//---------------------------------event joueur 2
 
 let bouton4 = document.querySelector(".bouton4");
 bouton4.addEventListener("click", function() {
-    Attaque(1, perso1, 1);
+    Uppercut(perso1);
     tremblement1();
     eclair2();
     afficherEVJ1();
@@ -137,7 +191,7 @@ bouton4.addEventListener("click", function() {
 
 let bouton5 = document.querySelector(".bouton5");
 bouton5.addEventListener("click", function() {
-    Attaque(2, perso1, 1);
+    Crochet(perso1);
     tremblement1();
     eclair2();
     afficherEVJ1();
@@ -149,7 +203,7 @@ bouton5.addEventListener("click", function() {
 
 let bouton6 = document.querySelector(".bouton6");
 bouton6.addEventListener("click", function() {
-    Attaque(4, perso1, 1);
+    Direct(perso1);
     tremblement1();
     eclair2();
     afficherEVJ1();
@@ -159,11 +213,11 @@ bouton6.addEventListener("click", function() {
     AfficherPoints2();
 })
 
-//event joueur 2
+// -----------------------------event joueur 1
 
 let bouton1 = document.querySelector(".bouton1");
 bouton1.addEventListener("click", function() {
-    Attaque(1, perso2, 1);
+    Uppercut(perso2);;
     tremblement2();
     eclair();
     afficherEVJ2();
@@ -175,7 +229,7 @@ bouton1.addEventListener("click", function() {
 
 let bouton2 = document.querySelector(".bouton2");
 bouton2.addEventListener("click", function() {
-    Attaque(2, perso2, 1);
+    Crochet(perso2);
     tremblement2();
     eclair();
     afficherEVJ2();
@@ -187,7 +241,7 @@ bouton2.addEventListener("click", function() {
 
 let bouton3 = document.querySelector(".bouton3");
 bouton3.addEventListener("click", function() {
-    Attaque(4, perso2, 1);
+    Direct(perso2);;
     tremblement2();
     eclair();
     afficherEVJ2();
